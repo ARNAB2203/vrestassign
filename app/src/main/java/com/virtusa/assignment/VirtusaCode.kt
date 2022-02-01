a9defd9f804f6f07221e55d86cc9f174069ae3db
Arnab Chatterjee <erarnabchatterjee@gmail.com>
2022-02-01 17:53:29 +0530

    Find unique items from an array.

--- app/src/main/java/com/virtusa/assignment/VirtusaCode.kt
+++ app/src/main/java/com/virtusa/assignment/VirtusaCode.kt
@@ -1,17 +1,39 @@
 package com.virtusa.assignment
+
 /**
-    1.
+1.
 
-    Write a program which takes two strings as input from the user (str1 and str2).
-    This program should print two strings as output (op1 and op2).
+Write a program which takes two strings as input from the user (str1 and str2).
+This program should print two strings as output (op1 and op2).
 
-    op1 should contain all the characters which are present in str1 but NOT present in str2.
-    op2 should contain all the characters which are present in str2 but NOT present in str1.
+op1 should contain all the characters which are present in str1 but NOT present in str2.
+op2 should contain all the characters which are present in str2 but NOT present in str1.
  */
 
 fun main() {
-    println(remove("ABC", "BC"))
-    println(remove("BC", "BANGALORE"))
+    findUnique(arrayOf(1, 2, 5, 2, 4, 5))
+}
+
+fun findUnique(array: Array<Int>) {
+    val map = HashMap<Int, Int>()
+
+    for (i in array.indices) {
+        print(array[i].toString() + " ")
+        if (map.containsKey(array[i])) {
+            val count = map[array[i]]
+            if (count != null) {
+                map[array[i]] = count + 1
+            }
+        } else {
+            map[array[i]] = 1
+        }
+    }
+
+    val filteredMap = map.filter {
+        it.value == 1
+    }
+
+    print(filteredMap.keys.toString()+" ")
 }
 
 fun remove(str1: String, str2: String): String? {
@@ -23,7 +45,7 @@ fun remove(str1: String, str2: String): String? {
         val ch1 = str1[j]
         var isMatched = false
         for (p in 0 until l2) {
-           val ch2 = str2[p]
+            val ch2 = str2[p]
             if (ch1 == ch2) {
                 isMatched = true
                 strUnmatch = strUnmatch.removePrefix(ch2.toString())
